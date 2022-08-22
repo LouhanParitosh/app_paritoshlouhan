ca624e562b205caea8792791c4272fcc65a84f71
Paritosh <paaritoshh@gmail.com>
2022-08-22 09:55:11 +0530

    Update Program.cs

--- NAGP-Assignment.Test/Program.cs
+++ NAGP-Assignment.Test/Program.cs
@@ -11,10 +11,7 @@ namespace NAGP_ASSIGNMENT.TEST
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
