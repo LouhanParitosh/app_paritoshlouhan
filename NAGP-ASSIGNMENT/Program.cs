8993e2f7357e2d8a3b62d0cbc18e6371e3d7096e
Paritosh Louhan <paritosh.louhan@nagarro.com>
2022-08-22 10:45:10 +0530

    bug fix

--- NAGP-ASSIGNMENT/Program.cs
+++ NAGP-ASSIGNMENT/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace NAGP_ASSIGNMENT
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
